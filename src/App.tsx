b7518c842743690777ec6fcbdb1f872ebdd44163
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-11 16:18:37 -0300

    feat: Ajusted Inicial Screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { MusicPlayer } from './components/MusicPlayer'
+
 export function App() {
   return (
-    <div>
-      <h1 className="text-cyan-600">Project Music Player</h1>
+    <div className="min-h-screen flex flex-row gap-8 justify-center items-center p-8 max-md:flex-col">
+      <MusicPlayer typePlayer="Player1" />
+      <div className="flex flex-col gap-8">
+        <MusicPlayer typePlayer="Player2" />
+        <MusicPlayer typePlayer="Player3" />
+      </div>
     </div>
   )
 }
