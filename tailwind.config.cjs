87644e298241c8f0fa486c7343fd18acb5ecbec9
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-11 16:14:33 -0300

    style: Created color pattern and add Font Robot.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        gray: {
+          200: '#E1E1E6',
+          400: '#C4C4CC',
+          900: '#0F0D13'
+        },
+        violet: {
+          900: '#2A2141'
+        }
+      }
+    }
   },
   plugins: []
 }
